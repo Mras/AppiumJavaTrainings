f0341fcb07e643bb11d08886f6fe5f9df8a9a96b
Pavlo Vedilin <pved@softserveinc.com>
2017-04-26 00:47:39 +0300

    lesson 3 preparations

--- src/test/java/HybridTests.java
+++ src/test/java/HybridTests.java
@@ -1,5 +1,10 @@
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -9,7 +14,15 @@ import java.util.Set;
 public class HybridTests extends AbstractTest{
     @Test
     public void hybridTest(){
-
+        MobileElement searchField = driver.findElementById("org.wikipedia:id/search_container");
+        searchField.sendKeys("Boeing 747");
+        WebDriverWait wait = new WebDriverWait(driver,5);
+        String firstTitleID = "org.wikipedia:id/page_list_item_title";
+        wait.until(ExpectedConditions.elementToBeClickable(By.id(firstTitleID)));
+        MobileElement firstTitle = driver.findElementById(firstTitleID);
+        Assert.assertEquals(firstTitle.getText(), "Boeing 747");
+        firstTitle.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("org.wikipedia:id/view_article_header_menu_bar")));
 
         printContexts();
     }
--- src/test/java/WebTests.java
+++ src/test/java/WebTests.java
@@ -1,16 +1,27 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import org.openqa.selenium.By;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
 
 public class WebTests extends AbstractTest{
     @Test
     public void webTest() {
-
+        driver.get("https://www.google.com");
+        MobileElement question = driver.findElement(By.cssSelector("input[name='q']"));
+        question.sendKeys("Advanced Appium lessons");
+        question.submit();
+        String firstResultTitle = driver.findElement(By.cssSelector("div.g:first-of-type h3 a")).getText();
+        assertTrue(firstResultTitle.contains("Advanced Appium"),"First result is not about Appium");
+        assertTrue(firstResultTitle.contains("PageObject"),"First result is not so interesting as it has nothing about PageObjects");
     }
 
     @BeforeClass
     public void init(){
         super.init();
-        
+        capabilities.setCapability(AndroidMobileCapabilityType.BROWSER_NAME, "Browser");
         startDriver();
     }
 }
